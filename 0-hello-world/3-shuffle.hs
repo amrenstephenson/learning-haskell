48292ce189b9965c23ca53cd969a1d762d675f0d
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-01-27 16:53:46 +0000

    Add shuffle

---
+++ 0-hello-world/3-shuffle.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use :" #-}
+
+main :: IO()
+
+shuffle :: [a] -> [a]
+shuffle [] = []
+shuffle (x : xs) = xs ++ [x]
+
+main = do
+    print (shuffle []::[Int])
+    print (shuffle [1])
+    print (shuffle [1, 2])
+    print (shuffle [1, 2, 3])
+    print (shuffle [1, 2, 3, 4])
+    print (shuffle [1, 2, 3, 4, 5])
