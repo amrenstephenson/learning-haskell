a52e6db505f1d106042a1905a54c614bb12fc458
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-01-27 18:48:18 +0000

    Add more comments

--- 1-types-and-lists/1-butlast.hs
+++ 1-types-and-lists/1-butlast.hs
@@ -1,12 +1,16 @@
 main :: IO()
 
 -- Using only built-in functions.
+-- Time complexity: O(n).
 butlast :: [a] -> a
 butlast xs = last (reverse (drop 1 (reverse xs)))
+-- More elegant alternative from the given solution:    butlast xs = head (reverse (tail xs))
+-- Which can be written in point-free style:            butlast' = head . reverse . tail
 
 -- Using only pattern matching.
+-- Time complexity: O(n).
 butlast' :: [a] -> a
-butlast' [a, b] = a
+butlast' [a, b] = a -- An alterntive is to replace b with _ as we have no need for it.
 butlast' (x:xs) = butlast' xs
 
 main = do
