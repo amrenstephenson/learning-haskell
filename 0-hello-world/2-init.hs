369e5f2973637a563c64835dcdf85ce0fd4c052b
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-01-27 16:42:42 +0000

    Add Tommy's suggestion

--- 0-hello-world/2-init.hs
+++ 0-hello-world/2-init.hs
@@ -11,8 +11,15 @@ init'' :: [a] -> [a]
 init'' [a] = []
 init'' (x:xs) = [x] ++ init' xs
 
+-- https://github.com/Muirey03's idea:
+init''' :: [a] -> [a]
+init''' xs = take (length xs - 1) xs
+
+
 main = do
     print (init' [1, 2, 3, 4, 5])
     print (init' [1])
     print (init'' [6, 7, 8, 9, 10])
     print (init'' [6])
+    print (init''' [11, 12, 13, 14, 15])
+    print (init''' [11])
